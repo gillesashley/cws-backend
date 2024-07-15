b440acf22ea3e1ae79701c4ac640366d3c1fb6b0
Gilles Ashley <xylaray37@gmail.com>
2024-07-15 20:12:37 +0000

    feat: implement authentication api with sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ConstituencyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +10,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/', function () {
-    return response()->json(['message' => 'Hello World!'], 200);
+    return response()->json(['message' => 'Welcome to campaign with us!'], 200);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('constituencies', [ConstituencyController::class]);
 });

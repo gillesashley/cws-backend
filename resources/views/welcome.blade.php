d53ad40fefc810edf55707046df3a7650cbf0861
Gilles Ashley <xylaray37@gmail.com>
2024-07-25 16:59:07 +0000

    setup base layout.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+@section('title', 'Welcome')
 
-<body>
-    <h2>Welcome to campaign with us @2024</h2>
-</body>
-
-</html>
+@section('content')
+    <div class="container">
+        <h1>Welcome</h1>
+    </div>
+@endSection

d8945ea6cd7d74c80cfc2aa8981ea0c3373dca23
Gilles Ashley <xylaray37@gmail.com>
2024-07-26 19:48:04 +0000

    removed bawumia image urls

--- database/factories/CampaignMessageFactory.php
+++ database/factories/CampaignMessageFactory.php
@@ -21,8 +21,8 @@ class CampaignMessageFactory extends Factory
     {
         $title = $this->faker->sentence;
         $imageUrls = [
-            'https://www.google.com/imgres?q=bawumia&imgurl=https%3A%2F%2Fwww.myjoyonline.com%2Fwp-content%2Fuploads%2F2023%2F08%2FDr-Bawumia.jpeg&imgrefurl=https%3A%2F%2Fwww.myjoyonline.com%2Fyour-demands-are-legitimate-and-cannot-be-ignored-bawumia-to-young-ghanaian-demonstrators%2F&docid=a1jrz8ML4gygkM&tbnid=6MIICeVVZH5g4M&vet=12ahUKEwiqk_b7u8WHAxXzW0EAHdTHIW0QM3oECF8QAA..i&w=1600&h=1600&hcb=2&ved=2ahUKEwiqk_b7u8WHAxXzW0EAHdTHIW0QM3oECF8QAA',
-            'https://www.google.com/imgres?q=bawumia&imgurl=https%3A%2F%2Fcitinewsroom.com%2Fwp-content%2Fuploads%2F2024%2F04%2FBAWUMIA.jpeg&imgrefurl=https%3A%2F%2Fcitinewsroom.com%2F2024%2F04%2Fbawumia-promises-100-ghanaian-ownership-of-natural-resources-if-elected-president%2F&docid=ZCsWQe7EEj2PkM&tbnid=cS7vtVw4Z5-kPM&vet=12ahUKEwiqk_b7u8WHAxXzW0EAHdTHIW0QM3oECGEQAA..i&w=1200&h=630&hcb=2&ved=2ahUKEwiqk_b7u8WHAxXzW0EAHdTHIW0QM3oECGEQAA',
+            'https://example.com/image1.jpg',
+            'https://example.com/image2.jpg',
             'https://example.com/image3.jpg',
             'https://example.com/image4.jpg',
             'https://example.com/image5.jpg',

2dfb613ede9e3912c40af3c5b925b6099efdd927
Gilles Ashley <xylaray37@gmail.com>
2024-07-28 01:31:01 +0000

    modify login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,72 +3,68 @@
 @section('title', 'Login')
 
 @section('content')
-    <div class="wrapper">
-        <div class="row g-0 m-0">
-            <div class="col-xl-6 col-lg-12">
-                <div class="login-cover-wrapper">
-                    <div class="card shadow-none">
-                        <div class="card-body">
-                            <div class="text-center">
-                                <h4>Sign In</h4>
-                                <p>Sign In to your account</p>
-                            </div>
-                            <form method="POST" action="{{ route('login') }}">
-                                @csrf
-                                <div class="col-12 mb-3">
-                                    <input type="email" class="form-control" name="email" placeholder="Email" required
-                                        autofocus>
-                                </div>
-                                <div class="col-12 mb-3">
-                                    <input type="password" class="form-control" name="password" placeholder="Password"
-                                        required>
-                                </div>
-                                <div class="col-12 col-lg-6 mb-3">
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember">
-                                        <label class="form-check-label" for="remember">Remember Me</label>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-lg-6 text-end mb-3">
-                                    <a href="{{ route('password.request') }}">Forgot Password?</a>
-                                </div>
-                                <div class="col-12 col-lg-12 mb-3">
-                                    <div class="d-grid">
-                                        <button type="submit" class="btn btn-primary">Sign In</button>
-                                    </div>
-                                </div>
-                            </form>
-                            <div class="col-12 col-lg-12">
-                                <div class="position-relative border-bottom my-3">
-                                    <div class="position-absolute seperator translate-middle-y">or continue with</div>
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-12">
-                                <div
-                                    class="social-login d-flex flex-row align-items-center justify-content-center gap-2 my-2">
-                                    <a href="javascript:;"><img src="{{ asset('assets/images/icons/facebook.png') }}"
-                                            alt=""></a>
-                                    <a href="javascript:;"><img
-                                            src="{{ asset('assets/images/icons/apple-black-logo.png') }}"
-                                            alt=""></a>
-                                    <a href="javascript:;"><img src="{{ asset('assets/images/icons/google.png') }}"
-                                            alt=""></a>
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-12 text-center">
-                                <p>Don't have an account? <a href="{{ route('register') }}">Sign up</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-6 col-lg-12">
-                <div class="position-absolute top-0 h-100 d-xl-block d-none login-cover-img">
-                    <div class="text-white p-5 w-100">
-                        <!-- You can add content here if needed -->
-                    </div>
-                </div>
+    <div class="text-center">
+        <h4>Sign In</h4>
+        <p>Sign In to your account</p>
+    </div>
+    <form class="form-body row g-3" method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="col-12">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
+                value="{{ old('email') }}" required autofocus>
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="col-12">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password"
+                name="password" required>
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="col-12 col-lg-6">
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="remember" name="remember"
+                    {{ old('remember') ? 'checked' : '' }}>
+                <label class="form-check-label" for="remember">Remember Me</label>
             </div>
         </div>
-    </div>
+        <div class="col-12 col-lg-6 text-end">
+            <a href="{{ route('password.request') }}">Forgot Password?</a>
+        </div>
+        <div class="col-12 col-lg-12">
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary">Sign In</button>
+            </div>
+        </div>
+        <div class="col-12 col-lg-12">
+            <div class="position-relative border-bottom my-3">
+                <div class="position-absolute seperator translate-middle-y">or continue with</div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-12">
+            <div class="social-login d-flex flex-row align-items-center justify-content-center gap-2 my-2">
+                <a href="javascript:;" class=""><img src="{{ asset('assets/images/icons/facebook.png') }}"
+                        alt=""></a>
+                <a href="javascript:;" class=""><img src="{{ asset('assets/images/icons/apple-black-logo.png') }}"
+                        alt=""></a>
+                <a href="javascript:;" class=""><img src="{{ asset('assets/images/icons/google.png') }}"
+                        alt=""></a>
+            </div>
+        </div>
+        <div class="col-12 col-lg-12 text-center">
+            <p class="mb-0">Don't have an account? <a href="{{ route('register') }}">Sign up</a></p>
+        </div>
+    </form>
+@endsection
+
+@section('cover-content')
+    <!-- You can add any content for the right side cover image here -->
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,14 +6,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- loader-->
     <link href="{{ asset('assets/css/pace.min.css') }}" rel="stylesheet" />
     <script src="{{ asset('assets/js/pace.min.js') }}"></script>
 
     <!--plugins-->
-    <link href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
 
     <!-- CSS Files -->
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -22,26 +23,43 @@
     <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 
-    <!--Theme Styles-->
-    <link href="{{ asset('assets/css/dark-theme.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/semi-dark.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/header-colors.css') }}" rel="stylesheet" />
-
-    <title>CWS - @yield('title', 'Authentication')</title>
+    <title>@yield('title', 'CWS - Authentication')</title>
 </head>
 
-<body>
+<body class="bg-white">
 
-    @yield('content')
+    <!--start wrapper-->
+    <div class="wrapper">
+        <div class="">
+            <div class="row g-0 m-0">
+                <div class="col-xl-6 col-lg-12">
+                    <div class="login-cover-wrapper">
+                        <div class="card shadow-none">
+                            <div class="card-body">
+                                @yield('content')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-12">
+                    <div class="position-absolute top-0 h-100 d-xl-block d-none login-cover-img">
+                        <div class="text-white p-5 w-100">
+                            @yield('cover-content')
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end row-->
+        </div>
+    </div>
+    <!--end wrapper-->
 
     <!-- JS Files-->
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <!-- Main JS-->
     <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    @stack('scripts')
 
 </body>
 

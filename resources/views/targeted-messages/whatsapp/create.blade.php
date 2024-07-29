dfe725521bf51a418d1e0acec79a0a7e05a5420b
Gilles Ashley <xylaray37@gmail.com>
2024-07-29 17:25:09 +0000

    users can preview images before sending

--- resources/views/targeted-messages/whatsapp/create.blade.php
+++ resources/views/targeted-messages/whatsapp/create.blade.php
@@ -22,6 +22,10 @@
                 <input type="file" name="media" id="media" class="form-control-file" accept="image/*,video/*">
             </div>
 
+            <div id="mediaPreview" class="mt-3" style="display: none;">
+                <img id="imagePreview" src="#" alt="Preview" style="max-width: 300px; max-height: 300px;">
+            </div>
+
             <div class="form-group">
                 <label>Recipients</label>
                 @foreach ($constituencyMembers as $member)
@@ -39,3 +43,22 @@
         </form>
     </div>
 @endsection
+
+
+@push('scripts')
+    <script>
+        document.getElementById('media').addEventListener('change', function(event) {
+            var file = event.target.files[0];
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                var img = document.getElementById('imagePreview');
+                img.src = e.target.result;
+                document.getElementById('mediaPreview').style.display = 'block';
+            }
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        });
+    </script>

bc173aa109c97b7432730fded31386ccf8eccef1
Kayfari <mr.osaeafari@gmail.com>
2024-08-08 11:02:56 +0000

    edited P&P transactions page

--- resources/views/points-and-payment/view-transaction.blade.php
+++ resources/views/points-and-payment/view-transaction.blade.php
@@ -1,6 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>View all Transactions</h2>
+    <!--start breadcrumb-->
+<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+    <div class="breadcrumb-title pe-3">Points & Payments</div>
+    <div class="ps-3">
+      <nav aria-label="breadcrumb">
+          <ol class="breadcrumb mb-0 p-0 align-items-center">
+              <li class="breadcrumb-item"><a href="javascript:;"><ion-icon name="receipt" role="img"
+                    class="md hydrated" aria-label="home outline"></ion-icon></a>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">Transactions</li>
+          </ol>
+      </nav>
+  </div>
+    <div class="ms-auto">
+        <div class="btn-group">
+            <button type="button" class="btn btn-outline-primary">Settings</button>
+            <button type="button"
+                class="btn btn-outline-primary split-bg-primary dropdown-toggle dropdown-toggle-split"
+                data-bs-toggle="dropdown"> <span class="visually-hidden">Toggle Dropdown</span>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-end"> <a class="dropdown-item"
+                    href="javascript:;">Action</a>
+                <a class="dropdown-item" href="javascript:;">Another action</a>
+                <a class="dropdown-item" href="javascript:;">Something else here</a>
+                <div class="dropdown-divider"></div> <a class="dropdown-item" href="javascript:;">Separated link</a>
+            </div>
+        </div>
+    </div>
+  </div>
+  <!--end breadcrumb-->
 
+  <div class="card radius-10 w-100">
+    <div class="card-body">
+        <div class="d-flex align-items-center">
+            <h6 class="mb-0">Payment Transactions</h6>
+            <div class="fs-5 ms-auto dropdown">
+                <div class="dropdown-toggle dropdown-toggle-nocaret cursor-pointer" data-bs-toggle="dropdown">
+                    <i class="bi bi-three-dots"></i>
+                </div>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </div><br>
+    </div>
+</div>
+
+ 
+</div> 
 @endsection
